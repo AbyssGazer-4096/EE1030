26a908a33fe16a890206d089f81de1ac9dc73478
Dwarak-A <you@example.com>
2024-09-21 11:03:30 +0530

    Add existing files

--- 3-3.2-19/codes/plot.py
+++ 3-3.2-19/codes/plot.py
@@ -1,47 +1,28 @@
 import numpy as np
-import numpy.linalg as LA
 import matplotlib.pyplot as plt
 
-# Load the data from the file
-data = np.loadtxt('points.dat')
-points = data[:3]
-line1 = data[3:24]
-line2 = data[24:]
+# Load the points from the text file
+def plot_triangle(filename, file_no=""):
+    points = np.loadtxt(filename, delimiter=',')
 
-A1 = np.array((points[0]))
-A2 = np.array((points[1]))
-B = np.array((points[2]))
+    # Extract the x and y coordinates
+    x = points[:, 0]
+    y = points[:, 1]
+    
+    # Plot the triangle
+    plt.figure()
+    plt.plot(x, y, label='Triangle Edges')
+    plt.fill(x, y, 'lightblue', alpha=0.5)
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.title("Triangle of side lengths " + str(round(((x[20] - x[0])**2 + (y[20] - y[0])**2)**0.5, 2)) + ", " +
+              str(round(((x[41] - x[21])**2 + (y[41] - y[21])**2)**0.5, 2)) + ", " +
+              str(round(((x[62] - x[42])**2 + (y[62] - y[42])**2)**0.5, 2)))
+    plt.grid(True)
+    plt.legend()
+    plt.axis('equal')
+    # Save the plot to figs directory
+    plt.savefig('../figs/fig' + str(file_no) + '.png')
+    plt.show()
 
-txtA1 = 'A\N{SUBSCRIPT ONE}'+str(tuple(A1))
-txtA2 = 'A\N{SUBSCRIPT TWO}'+str(tuple(A2))
-txtB = 'B'+str(tuple(B))
-
-# Split data into x and y
-x1 = line1[:, 0]
-y1 = line1[:, 1]
-x2 = line2[:, 0]
-y2 = line2[:, 1]
-
-# Plot the data
-plt.plot(x1, y1, label = 'A\N{SUBSCRIPT ONE}B')
-plt.plot(x2, y2, label = 'A\N{SUBSCRIPT TWO}B')
-
-plt.scatter(A1[0], A1[1], c = 'c', label = txtA1)
-plt.scatter(A2[0], A2[1], c = 'm', label = txtA2)
-plt.scatter(B[0], B[1], c = 'y', label = txtB)
-
-plt.annotate(txtA1, xy = A1)
-plt.annotate(txtA2, xy = A2)
-plt.annotate(txtB, xy = B)
-plt.annotate("10 units", xy = (A1+B)/2)
-plt.annotate("10 units", xy = (A2+B)/2)
-
-plt.xlabel('X-axis')
-plt.ylabel('Y-axis')
-plt.title('Points at a distance of 10 units from '+txtB)
-plt.axis('equal')
-plt.grid(True)
-plt.legend(loc = 'upper right')
-
-plt.savefig("/home/dwarak/MatrixTheory/EE1030/1-1.9-19/figs/plot.png")
-plt.show()
+plot_triangle('triangle.dat')
